3ab8255a9a12ec3877ba4ba0b1a691173294ea5d
Jakob Otto <jakob.otto@haw-hamburg.de>
2021-01-26 17:38:31 +0100

    Add script to run the probing parallely

--- python-dnssec/main.py
+++ python-dnssec/main.py
@@ -13,11 +13,6 @@ from tqdm import tqdm
 
 
 def test(domains):
-  with open('eggs.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-      print(', '.join(row))
-
   dnssec.validate_root_zone()
   for domain in domains:
     print('Checking:', domain)
@@ -28,6 +23,7 @@ if __name__ == '__main__':
   # , action='store_true' for boolean flags
   parser = argparse.ArgumentParser()
   parser.add_argument('--test', nargs='+', help='Domain(s) to validate')
+  parser.add_argument('input', help='The csv containing domains')
   parser.add_argument(
       '--output', help='The output path to write the csv to', default='../output/out.csv')
   args = parser.parse_args()
@@ -36,12 +32,11 @@ if __name__ == '__main__':
     test(args.test)
   else:
     dnssec.validate_root_zone()
-    alexa_df = pd.read_csv(
-        '../datasets/alexa-top1m-2021-01-04_0900_UTC.csv.tar.gz', sep=',', index_col=0, names=['domain'])[:-1]
-    with open(args.output, 'w', encoding='utf-8') as json_file:
-      for i, domain in enumerate(tqdm(alexa_df['domain'].values)):
-        result = dnssec.validate_chain(domain)
-        json.dump(result.as_dict(), json_file, ensure_ascii=False)
-        json_file.write('\n')
-        if (i % 10):
+    with open(args.input, 'r') as csv_file:
+      reader = csv.reader(csv_file)
+      with open(args.output, 'w', encoding='utf-8') as json_file:
+        for domain in tqdm(reader):
+          result = dnssec.validate_chain(domain[1])
+          json.dump(result.as_dict(), json_file, ensure_ascii=False)
+          json_file.write('\n')
           json_file.flush()
